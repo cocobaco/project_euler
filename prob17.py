6127e8b6d216aceb366a92af2e20352b94ccc3fd
cocobaco <aeroppon@gmail.com>
2020-10-24 14:30:16 +0700

    removed 20-twenty mapping from prob 17

--- prob17.py
+++ prob17.py
@@ -15,7 +15,7 @@ def num_to_text(num):
                         6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
     map_teens = {10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 
                  14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 
-                18: 'eighteen', 19: 'nineteen', 20: 'twenty'}
+                18: 'eighteen', 19: 'nineteen'}
     map_tens = {2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty', 
                 6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'}
         
